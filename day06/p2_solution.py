117728db5c6a4dbe464b9da45d03819d4dbab85e
miragekant <miragekant@outlook.com>
2021-12-12 16:32:49 -0800

    Update day 6 p2

---
+++ day06/p2_solution.py
@@ -0,0 +1,32 @@
+def solve():
+    result = 0
+
+    with open('input.txt') as f:
+        inputs = [int(t) for t in f.readlines()[0].strip().split(',')]
+
+        # Initialize the timer (counter)
+        timer = [0 for _ in range(9)] 
+        # Value ranges from 0 to 8
+        for i in range(9):
+            timer[i] = 0
+
+        for t in inputs:
+            timer[t] += 1
+
+        # Loop for 256 days
+        for _ in range(256):
+            spawn_count = timer[0]
+            for i in range(8, 0, -1):
+                timer[i - 1] = timer[i]
+            timer[6] += spawn_count
+            timer[8] += spawn_count
+        
+        result = sum(timer)
+
+    return result
+
+def main():
+    print(f'Answer: {solve()}')
+
+if __name__ == '__main__':
+    main()
